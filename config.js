a2573149a83bc92ad26603072551e1647da02119
charsijawaan <shakeelusama3@gmail.com>
2021-07-12 06:23:19 +0500

    API URL Added

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 let config = {};
 
-config.API_URL = 'http://192.168.100.6:3000/';
+// config.API_URL = 'http://192.168.100.6:3000/';
+config.API_URL = 'http://credittracker-env.eba-xmxnqa5p.us-east-2.elasticbeanstalk.com/';
 
 config.INTERNET_ISSUE_MSG = "Check Your Internet Connection.";
 config.FIELDS_MISSING_MSG = "Some Fields are Empty.";
